8e2e1fa5b5e14c1a7f42d446299461e93b77601f
Dereferenced-Null <lplace9001@gmail.com>
2022-09-15 14:41:26 +1000

    Update2

--- src/BattleshipPlayer.java
+++ src/BattleshipPlayer.java
@@ -16,6 +16,10 @@ public class BattleshipPlayer extends Thread{
     protected boolean done = false;
     private int initalPort;
 
+    private String [][] playerGame = new String[10][10];
+    private String [][] enemyGame = new String[10][10];
+    BattleShip[] battleShips = new BattleShip[5];
+
     //create player game
     //create enemy game
     //create battleship class
@@ -23,6 +27,12 @@ public class BattleshipPlayer extends Thread{
     public BattleshipPlayer(){
         Random rand = new Random();
         initalPort = rand.nextInt(5001,6000);
+        battleShips[0] = new BattleShip(5, "Aircraft Carrier");
+        battleShips[1] = new BattleShip(4, "Battleship");
+        battleShips[2] = new BattleShip(3, "Cruiser");
+        battleShips[3] = new BattleShip(3, "Submarine");
+        battleShips[4] = new BattleShip(2, "Patrol Boat");
+        createGame(playerGame, enemyGame);
     }
 
     public static void main(String [] args) {
@@ -71,21 +81,79 @@ public class BattleshipPlayer extends Thread{
         try{
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String input = "";
-            if(!reciever){
-                input = in.readLine();
-            }
-            else{
-                out.println("Start");
-            }
             //GAME LOOP
-            System.out.println(input);
+            playGame(reciever, in);
+            out.close();
+            in.close();
+
+
 
         }
         catch(Exception e){
             System.out.println(e);
             e.printStackTrace();
         }
+    }
+
+    private void playGame(boolean reciever, BufferedReader in){
+        String input = "";
+        printGames();
+        try{
+            while (!input.equals("GAME OVER"))
+                if(!reciever){
+                    //Receiving message
+                    input = in.readLine();
+                    reciever = false;
+                }
+                else{
+                    //Sending message
+                    //GAME LOOP
+
+                }
+        }
+        catch(Exception e){
+
+        }
+    }
+
+    private void createGame(String[][] playerGame, String[][] enemyGame){
+        Random rand = new Random();
+        for(int x = 0; x < 10; x ++){
+            for(int y = 0; y < 10; y++){
+                enemyGame[x][y] = "X";
+            }
+        }
+
+        int x = rand.nextInt(10);
+        int y = rand.nextInt(10);
+        for(int place = 0; place < 5; place ++){
+            if(playerGame[x][y] == null){
+                int size = battleShips[place]. getSize();
+                boolean flip = rand.nextBoolean();
+                //check if there is room on the x axis if boolean is true checking to make sure that x will not exceed 10 or go under 0
+                //if there is enough free spaces, place, otherwise, check if there is room on the y axis.
+                //if there is not, decrement place by 1, try again
+                //if placed then continue
+            }
+        }
+    }
+
+    private void printGames(){
+        System.out.print("YOUR GAME");
+        for(int x = 0; x < 10; x ++){
+            System.out.print("\n");
+            for(int y = 0; y < 10; y++){
+                System.out.print("["+playerGame[x][y]+ "]");
+            }
+        }
+        System.out.print("\n\nENEMY COMMANDERS GAME");
+        for(int x = 0; x < 10; x ++){
+            System.out.print("\n");
+            for(int y = 0; y < 10; y++){
+                System.out.print("["+enemyGame[x][y]+ "]");
+            }
+        }
+
     }
 
     private class Sender extends Thread{
@@ -103,7 +171,7 @@ public class BattleshipPlayer extends Thread{
                 InetAddress address = InetAddress.getByName("255.255.255.255");
                 socket.setBroadcast(true);
                 while(port == 0){
-                    Thread.sleep(30000);
+                    Thread.sleep(10); //should be 30000
                     for(int targetPort = 5000; targetPort < 6001; targetPort ++){
                         DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(), address, targetPort);
                         socket.send(packet);
@@ -160,5 +228,27 @@ public class BattleshipPlayer extends Thread{
             }
         }
     }
+
+    private class BattleShip{
+
+        //hitpoints, set on battleship creation
+        private int size;
+        private String [] coords;
+        private String name;
+
+        public BattleShip(int size, String name){
+            this.size = size;
+            this.name = name;
+        }
+
+        public int getSize(){
+            return size;
+        }
+
+        public void setCoords(int[][] XY){
+            //the coords representing where the ship is.
+        }
+    }
+
 
 }
